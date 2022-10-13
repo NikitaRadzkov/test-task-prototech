9cc9c183896816fbb172959db45b71c6f5c0fabe
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-10-13 09:35:21 +0300

    Add ListItemSeparator component

---
+++ app/components/lists/ListItemSeparator/ListItemSeparator.tsx
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+import React, { FC } from 'react';
+
+import styles from './styles';
+
+const ListItemSeparator: FC = () => {
+  return <View style={styles.separator} />;
+};
+
+export default ListItemSeparator;
