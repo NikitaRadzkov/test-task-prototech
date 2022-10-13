5d0e4491b2b3e04f62f7cc1b6044d5119e41e377
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-10-13 09:33:19 +0300

    Add TextInput component

---
+++ app/components/TextInput/TextInput.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+import { View, TextInput } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import defaultStyles from '../../config/styles';
+import styles from './styles';
+
+interface IAppTextInput {
+  icon: React.ComponentProps<typeof MaterialCommunityIcons>['name'];
+  width: string;
+}
+
+const AppTextInput: FC<IAppTextInput> = ({ icon, width = '100%', ...otherProps }) => {
+  return (
+    <View style={[styles.container, { width }]}>
+      {icon && <MaterialCommunityIcons name={icon} size={20} color={defaultStyles.colors.medium} style={styles.icon} />}
+      <TextInput placeholderTextColor={defaultStyles.colors.medium} style={defaultStyles.text} {...otherProps} />
+    </View>
+  );
+};
+
+export default AppTextInput;
