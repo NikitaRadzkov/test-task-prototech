6e4c4612c31412367b13da06cbe27279a5cb8a68
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-10-14 13:49:39 +0300

    Update list item

--- app/components/lists/ListItem/ListItem.tsx
+++ app/components/lists/ListItem/ListItem.tsx
@@ -1,24 +1,43 @@
 import { TouchableHighlight, View } from 'react-native';
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
 import colors from '../../../config/colors';
 import styles from './styles';
 import Text from '../../Text';
+import Card from '../../Card/Card';
+import { IQuotesTick } from '../../../types/data';
 
 interface IListItem {
-  title: string;
-  onPress: () => void;
+  symbol: string;
 }
 
-const ListItem: FC<IListItem> = (props) => {
-  const { title, onPress } = props;
+const ListItem: FC<IListItem> = ({ symbol }) => {
+  const [quotesTick, setQuotesTick] = useState<IQuotesTick>();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  useEffect(() => {
+    fetch(`https://quotes.instaforex.com/api/quotesTick?q=${symbol}`)
+      .then((response) => response.json())
+      .then((json) => {
+        setQuotesTick(json[0]);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
   return (
-    <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
-      <View style={styles.container}>
-        <Text>{title}</Text>
-      </View>
-    </TouchableHighlight>
+    <>
+      <TouchableHighlight
+        underlayColor={colors.light}
+        onPress={() => {
+          setIsModalVisible(true);
+        }}
+      >
+        <View style={styles.container}>
+          <Text>{symbol}</Text>
+        </View>
+      </TouchableHighlight>
+      <Card item={quotesTick} isModalVisible={isModalVisible} onPress={() => setIsModalVisible(false)} />
+    </>
   );
 };
 
