735e6a1f2247d14abc6a67431038b3b842065ead
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-10-16 20:35:44 +0300

    Remove extra import

--- app/screens/TradingInstrumentsWidget.tsx
+++ app/screens/TradingInstrumentsWidget.tsx
@@ -1,4 +1,4 @@
-import { FlatList, NativeSyntheticEvent, StyleSheet, TextInputChangeEventData, View } from 'react-native';
+import { FlatList, NativeSyntheticEvent, StyleSheet, TextInputChangeEventData } from 'react-native';
 import React, { FC, useEffect, useState } from 'react';
 
 import colors from '../config/colors';
